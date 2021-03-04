621650718d2691603e9c85233a37e2f6e15d9ec2
nathan <nhayfield@yahoo.com>
2021-03-04 15:51:02 -0500

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,18 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import ConnectForm from './pages/ConnectForm';
 
-const Hello2: FC = () => {
-  return (
-    <div>
-      <h3>HELLO 2</h3>
-    </div>
-  );
-};
+import {
+  createStyles,
+  jssPreset,
+  makeStyles,
+  StylesProvider,
+  ThemeProvider,
+} from '@material-ui/core';
+import { create } from 'jss';
+import { createTheme } from './utils/theme';
+import ConnectForm from './pages/ConnectForm';
+import { THEMES } from './utils/constants';
 
 const RouteListener: FC = (x) => {
   const history = useHistory();
@@ -25,22 +28,69 @@ const RouteListener: FC = (x) => {
   return <>{x.children}</>;
 };
 
-export default function App() {
-  return (
-    <HashRouter>
-      <Switch>
-        <RouteListener>
-          <Route exact path="/">
-            <Redirect to="/connect" />
-          </Route>
-          <Route exact path="/hello2" component={Hello2} />
-          <Route exact path="/connect" component={ConnectForm} />
-          <Route
-            path="/edit_connect/:channelId/:editingConnected"
-            component={ConnectForm}
-          />
-        </RouteListener>
-      </Switch>
-    </HashRouter>
-  );
+const jss = create({ plugins: [...jssPreset().plugins] });
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    '@global': {
+      '*': {
+        boxSizing: 'border-box',
+        margin: 0,
+        padding: 0,
+      },
+      html: {
+        '-webkit-font-smoothing': 'antialiased',
+        '-moz-osx-font-smoothing': 'grayscale',
+        height: '100%',
+        width: '100%',
+      },
+      body: {
+        height: '100%',
+        width: '100%',
+      },
+      '#root': {
+        height: '100%',
+        width: '100%',
+      },
+    },
+  })
+);
+
+interface Settings {
+  direction?: 'ltr' | 'rtl';
+  responsiveFontSizes?: boolean;
+  theme?: string;
+  rowCount?: number;
 }
+
+const defaultSettings: Settings = {
+  responsiveFontSizes: true,
+  theme: THEMES.LIGHT,
+  rowCount: 25,
+};
+
+const App: FC = () => {
+  useStyles();
+  return (
+    <ThemeProvider theme={createTheme(defaultSettings)}>
+      <StylesProvider jss={jss}>
+        <HashRouter>
+          <Switch>
+            <RouteListener>
+              <Route exact path="/">
+                <Redirect to="/connect" />
+              </Route>
+              <Route exact path="/connect" component={ConnectForm} />
+              <Route
+                path="/edit_connect/:channelId/:editingConnected"
+                component={ConnectForm}
+              />
+            </RouteListener>
+          </Switch>
+        </HashRouter>
+      </StylesProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
