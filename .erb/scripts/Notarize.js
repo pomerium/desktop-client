c817401f4ccd69a66cd26300bef96a543c1002d9
Travis Groth <travisgroth@users.noreply.github.com>
2021-09-03 17:04:16 -0400

    Set up GH release publishing

--- .erb/scripts/Notarize.js
+++ .erb/scripts/Notarize.js
@@ -12,19 +12,33 @@ exports.default = async function notarizeMacos(context) {
     return;
   }
 
-  if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
+  if (
+    !('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env) &&
+    !('APPLE_ID' in process.env && 'APPLE_ID_KEY_ISSUER' in process.env)
+  ) {
     console.warn(
-      'Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set'
+      'Skipping notarizing step. APPLE_ID and APPLE_ID_PASS or APPLE_ID_KEY_ISSUER env variables must be set'
     );
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
-  await notarize({
-    appBundleId: build.appId,
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
-  });
+  // Password based auth
+  if ('APPLE_ID_PASS' in process.env) {
+    await notarize({
+      appBundleId: build.appId,
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASS,
+    });
+    // API key based auth
+  } else {
+    await notarize({
+      appBundleId: build.appId,
+      appPath: `${appOutDir}/${appName}.app`,
+      appleApiKey: process.env.APPLE_ID,
+      appleApiIssuer: process.env.APPLE_ID_KEY_ISSUER,
+    });
+  }
 };
