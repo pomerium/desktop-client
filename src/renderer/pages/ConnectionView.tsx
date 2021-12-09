4a694e2cdc219437acdd3c9c24cdc0d00ae8e3df
nathan <nhayfield@yahoo.com>
2021-12-09 11:09:16 -0500

    remove unused import

--- src/renderer/pages/ConnectionView.tsx
+++ src/renderer/pages/ConnectionView.tsx
@@ -40,7 +40,6 @@ import Delete from '../icons/Delete';
 import {
   Connection,
   ConnectionStatusUpdate,
-  ConnectionStatusUpdate_ConnectionStatus,
   ListenerUpdateRequest,
   Record,
   Selector,
