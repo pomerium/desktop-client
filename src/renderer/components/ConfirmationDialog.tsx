19e2ab4b1d919b848647724473a8b13d5e74dae0
Denis Mishin <dmishin@pomerium.com>
2022-01-10 17:50:55 -0500

    tag delete: confirmation dialog

---
+++ src/renderer/components/ConfirmationDialog.tsx
@@ -0,0 +1,42 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from '@material-ui/core';
+import React, { FC } from 'react';
+
+export interface ConfirmationDialogProps {
+  title: string;
+  text: string;
+  onConfirm: () => void;
+  onClose: () => void;
+}
+
+const ConfirmationDialog: FC<ConfirmationDialogProps> = ({
+  title,
+  text,
+  onConfirm,
+  onClose,
+}) => {
+  return (
+    <Dialog open={true} onClose={onClose}>
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {text}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={onConfirm} autoFocus>
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmationDialog;
