a88f6eba9b95160338397d096f0c5edd73e81e07
Denis Mishin <dmishin@pomerium.com>
2022-01-10 18:45:48 -0500

    lint

--- src/renderer/components/ConfirmationDialog.tsx
+++ src/renderer/components/ConfirmationDialog.tsx
@@ -1,3 +1,4 @@
+import React, { FC } from 'react';
 import {
   Dialog,
   DialogTitle,
@@ -6,8 +7,6 @@ import {
   DialogActions,
   Button,
 } from '@material-ui/core';
-import React, { FC } from 'react';
-
 export interface ConfirmationDialogProps {
   title: string;
   text: string;
@@ -20,23 +19,21 @@ const ConfirmationDialog: FC<ConfirmationDialogProps> = ({
   text,
   onConfirm,
   onClose,
-}) => {
-  return (
-    <Dialog open onClose={onClose}>
-      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          {text}
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={onConfirm} autoFocus>
-          OK
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-};
+}: ConfirmationDialogProps): JSX.Element => (
+  <Dialog open onClose={onClose}>
+    <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">
+        {text}
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={onClose}>Cancel</Button>
+      <Button onClick={onConfirm} autoFocus>
+        OK
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
 
 export default ConfirmationDialog;
