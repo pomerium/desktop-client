dc31d0754b82707dadf7412e38b935f58ea665d6
Caleb Doxsey <cdoxsey@pomerium.com>
2025-01-23 14:55:40 -0700

    lint

--- src/renderer/components/NestedAccordion.tsx
+++ src/renderer/components/NestedAccordion.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { Accordion, AccordionProps, styled } from '@mui/material';
 import React from 'react';
 
--- src/renderer/components/NestedAccordionSummary.tsx
+++ src/renderer/components/NestedAccordionSummary.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { AccordionSummary, AccordionSummaryProps, styled } from '@mui/material';
 import React from 'react';
 import { ChevronRight } from 'react-feather';
